ccdd4598f55498a0f1375f23185944683d30bab8
Milo-F <milo.fox168@gmail.com>
2022-05-27 12:20:37 +0800

    updata

---
+++ crc.v
@@ -0,0 +1,62 @@
+module crc (
+    input       wire                                        clk,
+    input       wire                                        rst_n,
+    input       wire        [31:0]                          data_in,
+    output      reg         [39:0]                          data_out
+);
+    
+    wire                    [39:0]                          data_out_nxt;
+    wire                    [39:0]                          data;
+    assign data              = {data_in, 8'b0};
+    localparam [8:0] crc_num = 8'b1_0011_0001;
+    wire                    [7:0]                           crc_out;
+    wire                    [8:0]                           crc_0,crc_1,crc_2,crc_3,crc_4,crc_5,crc_6,crc_7,crc_8,crc_9,crc_10,crc_11,crc_12,crc_13,crc_14,crc_15,crc_16,crc_17,crc_18,crc_19,crc_20,crc_21,crc_22,crc_23,crc_24,crc_25,crc_26,crc_27,crc_28,crc_29,crc_30,crc_31;
+    assign crc_0   = data[39:31] ^ (data[39] ? crc_num : 9'b0);
+    assign crc_1   = {crc_0[7:0], data[30]} ^ (crc_0[7] ? crc_num : 9'b0);
+    assign crc_2   = {crc_1[7:0], data[29]} ^ (crc_1[7] ? crc_num : 9'b0);
+    assign crc_3   = {crc_2[7:0], data[28]} ^ (crc_2[7] ? crc_num : 9'b0);
+    assign crc_4   = {crc_3[7:0], data[27]} ^ (crc_3[7] ? crc_num : 9'b0);
+    assign crc_5   = {crc_4[7:0], data[26]} ^ (crc_4[7] ? crc_num : 9'b0);
+    assign crc_6   = {crc_5[7:0], data[25]} ^ (crc_5[7] ? crc_num : 9'b0);
+    assign crc_7   = {crc_6[7:0], data[24]} ^ (crc_6[7] ? crc_num : 9'b0);
+    assign crc_8   = {crc_7[7:0], data[23]} ^ (crc_7[7] ? crc_num : 9'b0);
+    assign crc_9   = {crc_8[7:0], data[22]} ^ (crc_8[7] ? crc_num : 9'b0);
+    assign crc_10  = {crc_9[7:0], data[21]} ^ (crc_9[7] ? crc_num : 9'b0);
+    assign crc_11  = {crc_10[7:0], data[20]} ^ (crc_10[7] ? crc_num : 9'b0);
+    assign crc_12  = {crc_11[7:0], data[19]} ^ (crc_11[7] ? crc_num : 9'b0);
+    assign crc_13  = {crc_12[7:0], data[18]} ^ (crc_12[7] ? crc_num : 9'b0);
+    assign crc_14  = {crc_13[7:0], data[17]} ^ (crc_13[7] ? crc_num : 9'b0);
+    assign crc_15  = {crc_14[7:0], data[16]} ^ (crc_14[7] ? crc_num : 9'b0);
+    assign crc_16  = {crc_15[7:0], data[15]} ^ (crc_15[7] ? crc_num : 9'b0);
+    assign crc_17  = {crc_16[7:0], data[14]} ^ (crc_16[7] ? crc_num : 9'b0);
+    assign crc_18  = {crc_17[7:0], data[13]} ^ (crc_17[7] ? crc_num : 9'b0);
+    assign crc_19  = {crc_18[7:0], data[12]} ^ (crc_18[7] ? crc_num : 9'b0);
+    assign crc_20  = {crc_19[7:0], data[11]} ^ (crc_19[7] ? crc_num : 9'b0);
+    assign crc_21  = {crc_20[7:0], data[10]} ^ (crc_20[7] ? crc_num : 9'b0);
+    assign crc_22  = {crc_21[7:0], data[9]} ^ (crc_21[7] ? crc_num : 9'b0);
+    assign crc_23  = {crc_22[7:0], data[8]} ^ (crc_22[7] ? crc_num : 9'b0);
+    assign crc_24  = {crc_23[7:0], data[7]} ^ (crc_23[7] ? crc_num : 9'b0);
+    assign crc_25  = {crc_24[7:0], data[6]} ^ (crc_24[7] ? crc_num : 9'b0);
+    assign crc_26  = {crc_25[7:0], data[5]} ^ (crc_25[7] ? crc_num : 9'b0);
+    assign crc_27  = {crc_26[7:0], data[4]} ^ (crc_26[7] ? crc_num : 9'b0);
+    assign crc_28  = {crc_27[7:0], data[3]} ^ (crc_27[7] ? crc_num : 9'b0);
+    assign crc_29  = {crc_28[7:0], data[2]} ^ (crc_28[7] ? crc_num : 9'b0);
+    assign crc_30  = {crc_29[7:0], data[1]} ^ (crc_29[7] ? crc_num : 9'b0);
+    assign crc_31  = {crc_30[7:0], data[0]} ^ (crc_30[7] ? crc_num : 9'b0);
+    assign crc_out = crc_31[7:0];
+    
+    
+    
+    
+    assign data_out_nxt = {data_in, crc_out};
+    
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            data_out <= 0;
+        end
+        else begin
+            data_out <= data_out_nxt;
+        end
+    end
+    
+endmodule
--- creat_ins.py
+++ creat_ins.py
@@ -1,83 +1,84 @@
 #! /usr/bin/python3
-
-from distutils import filelist
-from fileinput import filename
-import os
+# -*- coding: utf-8 -*-
 import re
 import sys, getopt
-from venv import create
-# dir_path = './'
-# file_name = sys.argv[1]
-# print(file_name)
-# # file_list = os.listdir(dir_path)
-# with open(file_name, 'r') as f:
-#     f_content = f.read()
-# ano = re.findall(r' *//.*\n', f_content)
-# # print(ano)
-# for str in ano:
-#     f_content = f_content.replace(str.replace('\n',''), '')
-# first_p = f_content.find('module')+6
-# end_p = f_content.find(';')+1
-# f_content = f_content[first_p:end_p]
-# # print(f_content)
-# for i in range(0,len(f_content)):
-#     if f_content[i] != ' ':
-#         first_p = i
-#         break
-#     i += 1
-# f_content = f_content[first_p:end_p]
-# have_para = True if (f_content.find(')') < f_content.find('input') or f_content.find(')') < f_content.find('output')) else False
-# f_list = list(f_content)
-# # print(have_para)
-# # print(f_content)
-# if not have_para :
-#     ins_name_p = f_content.find('(')
-#     mod_name = f_content[0:ins_name_p].replace(' ','')
-#     ins_name = mod_name+'_ins '
-#     f_list.insert(ins_name_p, ins_name)
-# else:
-#     ins_name_p = f_content.find('#')
-#     mod_name = f_content[0:ins_name_p].replace(' ','')
-#     ins_name = mod_name+'_ins '
-#     index = f_content.find(')')+f_content[f_content.find(')'):].find('(')
-#     f_list.insert(index, ins_name)
-# f_content = ''.join(f_list)
-# print(f_content)
-# f_content = re.sub(r'\B ?inout *(?:wire|reg)? *(?:\[.*\])? *|\B ?output *(?:wire|reg)? *(?:\[.*\])? *|\B ?input *(?:wire)? *(?:\[.*\])? *','.',f_content)
-# port_name = re.findall(r'\B\..*,?', f_content)
-# print(port_name[-6])
-# for str in port_name:
-#     name = str.replace('.','').replace(',','')
-#     end_dot = ',' if str.find(',') >0 else ''
-#     f_content = f_content.replace(str, ' '+str.replace(',', '')+'('+name+')'+end_dot)
-# print(file_name.replace('.v', '_ins.v'))
-# os.system('touch '+file_name.replace('.v', '_ins.v'))
-# with open(file_name.replace('.v', '_ins.v'), 'w') as f:
-#     f.write(f_content)
-#     pass
+
+def get_help():
+    print("usage: create_ins.py -f file_name <-o out_file> <-i ins_name>")
+    print("Options and arguments:")
+    print("\t-f --file_name=\t: input verilog file with one module inside")
+    print("\t-o --out_file=\t: instance code output file")
+    print("\t-i --ins_name=\t: input a instance name you want")
+    print("\t-h --help=\t: get help")
+    sys.exit()
 
 def create_ins(argv):
+    module_name = "" # 模块名字
+    ins_name = "" # 实例名字
+    given_ins_name = False # 是否指定了实例名字
+    module_def = "" # 模块定义语句块
+    params_def = "" # 参数定义语句块
+    ports_def = "" # 端口定义语句块
+    param_exist = False # 是否是参数化的模块
+    output_list = [] # 输出实例调用
+    param_list = [] # 参数名列表
+    ports_list = [] # 端口名列表
     if len(argv) == 0:
         print("Error: missing parameters!")
         sys.exit()
     try:
-        opts, args = getopt.getopt(argv, "hf:o:", ["help", "file_name", "out_file"])
+        opts, args = getopt.getopt(argv, "hf:o:i:", ["help", "file_name=", "out_file=", "ins_name="])
     except:
         print("Error: get parameters error!")
         sys.exit(2)
     for opt, arg in opts:
         if opt in ("-h", "--help"):
-            print("usage: create_ins.py -f file_name <-o out_file>")
-            print("Options and arguments:")
-            print("\t-f --file_name\t: input verilog file with one module inside")
-            print("\t-o --out_file\t: instance code output file")
-            print("\t-h --help\t: get help")
-        
+            get_help()
+
         if opt in ("-f", "--file_name"):
-            pass
-
+            input_file = arg
+        output_file = input_file.replace(".v", ".inst")
         if opt in ("-o", "--out_file"):
+            output_file = arg
+        if opt in ("-i", "--ins_name") :
+            given_ins_name = True
+            ins_name = arg
+
+    with open(input_file, "r") as f:
+        # 提取module name
+        for line in f.readlines():
+            line = line.strip()
+            # 判断参数是否存在
+            if re.compile(r"#").search(line):
+                param_exist = True
+            # 包含关键字module
+            if re.compile(r"\bmodule\b").search(line):
+                # 包含"("或者"#("
+                if re.compile(r"[#]?\(").search(line):
+                    module_def = re.search(r"\Amodule\b *\w*\b", line).group()
+                    break
+                else:    
+                    module_def = module_def + line
+            else:
+                if re.compile(r"[#]?\(").search(line):
+                    if re.compile(r"\w*").search(line):
+                        module_def = module_def + " " + re.search(r"\w*", line).group()
+                    break
+                else:
+                    module_def = module_def + " " + line
+        module_name = re.search(r"\b\w*\b\Z", module_def).group()
+        if not given_ins_name:
+            ins_name = module_name + "_ins"
+        print(ins_name)
+
+        # 提取参数列表
+        print (param_exist)
+        if param_exist:
             pass
 
+
+        f.close()
+
+# mian
 if __name__ == "__main__":
     create_ins(sys.argv[1:])
